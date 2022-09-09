2ef085c52ca7f0f548d61db0e9686e65afcb6e81
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-09-09 17:57:25 +0200

    added navigation links

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,21 @@ import { Container } from "react-bootstrap"
 import { Home } from "./pages/Home"
 import { Store } from "./pages/Store"
 import { About } from "./pages/About"
+import { Navbar } from "./components/Navbar"
 
 function App() {
-  return <Container>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/store" element={<Store />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </Container>
+  return (
+    <>
+      <Navbar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  )
 }
 
 export default App
