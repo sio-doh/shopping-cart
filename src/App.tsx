12cbc4625195a77dfaf041fc04482cfeef3ebbb5
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-09-09 16:33:48 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { Routes, Route } from "react-router-dom"
+import { Container } from "react-bootstrap"
+
 function App() {
-  return <h1>Tinkering with Vite, React and TypeScript</h1>
+  // return <Container>Tinkering with Vite, React and TypeScript</Container>
+  return <Container>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/store" element={<Store />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Container>
 }
 
 export default App
