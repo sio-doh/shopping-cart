c7663385f7d1841075904db042c255d41b3bdf38
Siobhan Doherty <siobhandoherty18@gmail.com>
2022-09-15 19:55:36 +0200

    cart items and shopping cart components

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useContext, useState } from "react"; 
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
     children: ReactNode
@@ -92,7 +93,8 @@ export function ShoppingCartProvider({ children }:
                 cartQuantity
         }}
         > 
-            {children}
+            {children} 
+            <ShoppingCart isOpen={isOpen}/>
         </ShoppingCartContext.Provider>
     )
 }
